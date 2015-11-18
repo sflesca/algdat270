f5998b74e62984154f4cbaab8ada2b8fb9d1292d
Sergio Flesca <sflesca@hotmail.com>
2015-11-18 19:05:31 +0100

    altro

--- algoritmi270/src/hashmap/TabellaHash.java
+++ algoritmi270/src/hashmap/TabellaHash.java
@@ -1,5 +1,7 @@
 package hashmap;
 
+import java.util.LinkedList;
+
 public class TabellaHash<PK,T> {
 	
 	protected class Pair <PK, T>{
@@ -28,9 +30,41 @@ public class TabellaHash<PK,T> {
 		public void setObj(T obj) {
 			this.obj = obj;
 		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + ((key == null) ? 0 : key.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Pair other = (Pair) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (key == null) {
+				if (other.key != null)
+					return false;
+			} else if (!key.equals(other.key))
+				return false;
+			return true;
+		}
+
+		private TabellaHash<PK,T> getOuterType() {
+			return (TabellaHash<PK, T>) TabellaHash.this;
+		}
 	}
 	
-	Pair<PK,T>[] buckets; 
+	LinkedList<Pair<PK,T>>[] buckets; 
 	
 	int size=0;
 
