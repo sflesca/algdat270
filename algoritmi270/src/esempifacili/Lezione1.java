c79179ba39d650d4b81a699989d61b945906378d
sfles <sfles@DESKTOP-HJVUEKL.homenet.telecomitalia.it>
2020-03-16 13:39:23 +0100

    esempi

--- algoritmi270/src/esempifacili/Lezione1.java
+++ algoritmi270/src/esempifacili/Lezione1.java
@@ -1,11 +1,46 @@
 package esempifacili;
 
+import java.util.Arrays;
+
 public class Lezione1 {
 
 	public Lezione1() {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	// lato matrice n
+	// dimensioni interi b
+	// dimensione dell'input b * n^2
+	// parametro rappresentativo della dimensione dell'input n
+	public static int[][] sommaMatriciQuadrate(int[][] A, int[][] B){
+		if((A.length!=A[0].length)||(A.length!=B.length)||(A[0].length!=B[0].length))
+			throw new RuntimeException();
+		int[][] res = new int[A.length][A[0].length];
+		for(int i = 0; i<A.length; i++) // proporzionali a n
+			for(int j = 0;j<A[0].length;j++) // proporzionali a n
+				res[i][j] = A[i][j]+B[i][j]; // proporzionale a n*n complessità theta(n^2)
+		return res;
+	}
+	
+	// lato matrice n
+	// dimensioni interi b
+	// dimensione dell'input b * n^2 D = n^2 => n=D^(1/2) theta(n^3) => theta((D^(1/2))^3) = theta(D^(3/2))
+	// parametro rappresentativo della dimensione dell'input n
+	public static int[][] prodottoMatriciQuadrate(int[][] A, int[][] B){
+		if((A.length!=A[0].length)||(A.length!=B.length)||(A[0].length!=B[0].length))
+			throw new RuntimeException();
+		int[][] res = new int[A.length][A[0].length];
+		for(int i = 0; i<A.length; i++) // proporzionali a n
+			for(int j = 0;j<B[0].length;j++) { // proporzionali a n
+				res[i][j] =0;
+				for(int k=0; k<A.length;k++) // proporzionali a n complessità theta(n^3)
+					res[i][j] += A[i][k]*B[k][j];
+			}
+		return res;
+	}
+	
+	
 	// n (dimensione dell'input) è il numero di celle in v
 	// caso migliore 4 istruzioni Theta(1)
 	// caso peggiore 3 + 3 *n Theta(n)----- theta(n)
@@ -16,6 +51,9 @@ public class Lezione1 {
 		return false;
 	}
 	
+	
+	// dim input D = lg_2(n) => n = 2^D
+	// complessità spaziale theta(n) => theta(2^D)
 	public static int fatt(int n){
 		if (n<=1) return 1;
 		return n* fatt(n-1);
@@ -81,9 +119,26 @@ public class Lezione1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] v = {0,1,2,7,4,5};
-		System.out.println(subsetSum(v,6));
-		System.out.println(subsetSum(v,20));
+		
+		int f = fibRic(5);
+		System.out.println(f);
+		
+//		int[][] A = {{0,1,2},
+//					 {1,2,3},
+//					 {2,3,4}};
+//		int[][] B = {{0,1,2},
+//					 {1,2,3},
+//					 {2,3,4}};
+//		
+//		int[][] R = sommaMatriciQuadrate(A,B);
+//		System.out.println(Arrays.deepToString(R));
+//		
+//		R = prodottoMatriciQuadrate(A,B);
+//		System.out.println(Arrays.deepToString(R));
+		
+//		int[] v = {0,1,2,7,4,5};
+//		System.out.println(subsetSum(v,6));
+//		System.out.println(subsetSum(v,20));
 	}
 
 }
